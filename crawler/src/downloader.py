62271d0e78802da0443720cc62798a40bb718e06
bambino25 <xleopold.paris@gmail.com>
2025-03-11 14:45:40 +0100

    third commit

---
+++ crawler/src/downloader.py
@@ -0,0 +1,14 @@
+class Downloader:
+    def __init__(self, headers=None):
+        self.headers = headers or {
+            'User-Agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/58.0.3029.110 Safari/537.3'
+        }
+
+    def download(self, url):
+        try:
+            response = requests.get(url, headers=self.headers)
+            response.raise_for_status()
+        except requests.exceptions.RequestException as e:
+            print(e)
+            return None
+        return
